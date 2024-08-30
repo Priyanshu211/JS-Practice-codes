b667a049b2118b4e0d5fcae6a8aa073c0d77db1c
Priyanshu211 <87142123+Priyanshu211@users.noreply.github.com>
2024-08-30 16:16:16 +0000

    Edited the files and added new things

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountID = 12934;
+let accountEmail = "priyanshutripathi@gmail.com";
+var accountPassword = "mynameis";
+accountCity = "Delhi";
+
+console.table([accountID,accountEmail,accountPassword,accountCity]);
+
+// if we dont give value to any value we get undefined value
+
+// do not use var because of functional and scope problems
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict" // treat code as newer version of js
+
+// alert(3+3) we are using nodejs, not browser;
+
+// increase code readability 
+
+let name = "priyanshu";
+
+let age = 20;
+
+let isbool = 0;
+
+// bigint
+// string
+// boolean
+// null => standalone value (empty)
+// undefined => 
+// symbol => uniquess 
+
+
+// object 
+
+console.log(typeof(null));
+// object will be the answer in this case 
+
+
+
+
